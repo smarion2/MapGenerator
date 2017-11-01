36dc1af97738fe0d231aea0847965bd57f286292
Sam Marion <smarion2@gmail.com>
2017-11-01 00:13:23 -0400

    basic structure down

---
+++ MapGenerator/Cave.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapGenerator
+{
+    public class Cave : IMap
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public int SquareSize { get; private set; }
+
+        private int BirthChance;
+        private int BirthLimit;
+        private int DeathLimit;
+
+        private int[,] Squares;
+
+        public Cave(int width, int height, int squareSize, int birthChance, int birthLimit, int deathLimit)
+        {
+            Width = width;
+            Height = height;
+            squareSize = SquareSize;
+            Squares = new int[width, height];
+            BirthChance = birthChance;
+            BirthLimit = birthLimit;
+            DeathLimit = deathLimit;
+        }
+
+        public void GenerateMap()
+        {
+
+        }
+    }
+}
---
+++ MapGenerator/iMap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapGenerator
+{
+    interface IMap
+    {
+        int Width { get; }
+        int Height { get; }
+        int SquareSize { get; }
+
+        void GenerateMap();
+        
+    }
+}
